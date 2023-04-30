fd32d662206d441c038d99e3c572a7aac642a50f
Marko Varga <marko.varga@fer.hr>
2023-04-30 11:50:26 +0200

    riješeno

--- zadace/nabojiDZ.jl
+++ zadace/nabojiDZ.jl
@@ -4,70 +4,96 @@ import Distributions: Uniform
 eps0 = 8.854e-12
 k = 1 / (4 * π * eps0)
 
-function elektricnoPolje(koordinateNaboja, iznosNaboja, xG, yG, N)
-    n = Int(sizeof(koordinateNaboja[:, 1]) / 8)
-    Ex = Ey = zeros(N, N)
-    for i = 1:n
-        Rx = xG .- koordinateNaboja[i, 1]
-        Ry = yG .- koordinateNaboja[i, 2]
-        R = sqrt.(Rx .^ 2 + Ry .^ 2) .^ 3
-        Ex += k .* iznosNaboja[i] .* Rx ./ R
-        Ey += k .* iznosNaboja[i] .* Ry ./ R
+# funkcija za odrešivanje polja u svakoj točki mreže
+function elektricnoPolje(koordinateNaboja, iznosNaboja, xG, yG, N, n, m)
+    # inicijalizacija polja
+    Ex = zeros(N, N)
+    Ey = zeros(N, N)
+    E = zeros(N, N)
+    # iteriramo kroz svaku točku i koristimo funkciju poljeUTocki kako bismo
+    # polje u pojedinoj točki
+    for i = 1:N
+        for j = 1:N
+            E_ij, Ex_ij, Ey_ij = poljeUTocki(koordinateNaboja, iznosNaboja, xG[i, j], yG[i, j], n, m)
+            E[i, j] = E_ij
+            Ex[i, j] = Ex_ij
+            Ey[i, j] = Ey_ij
+        end
     end
-    return Ex, Ey
+    return E, Ex, Ey    # vraćamo ukupni vektore polja kao i rastav na x i y komponente
+end
+
+# funkcija za određivanje električnog polja u jednoj točki
+function poljeUTocki(koordinateNaboja, iznosNaboja, x, y, n, m)
+    Ex = Ey = 0     #početne vrijednosti polja
+    # dodajemo utjecaj svakog polja po načelu superpozicije
+    for i = 1:(n+m)
+        Rx = x - koordinateNaboja[i, 1]
+        Ry = y - koordinateNaboja[i, 2]
+        R = sqrt(Rx^2 + Ry^2)^3
+        Ex += k * (i < n ? iznosNaboja : -iznosNaboja) * Rx / R
+        Ey += k * (i < n ? iznosNaboja : -iznosNaboja) * Ry / R
+    end
+    println(string(Ex) * " " * string(Ey))
+    return sqrt(Ex^2 + Ey^2), Ex, Ey  # vraćamo ukupni iznos polja kao i rastav na x i y komponentu
+end
+
+
+# funkcija za generiranje mreže
+function mreza(koordinateNaboja, N)
+    # određivanje granica
+    minX = floor(minimum(koordinateNaboja[:, 1]) * 2)
+    maxX = ceil(maximum(koordinateNaboja[:, 1]) * 2)
+    minY = floor(minimum(koordinateNaboja[:, 2]) * 2)
+    maxY = ceil(maximum(koordinateNaboja[:, 2]) * 2)
+    x = range(minX, maxX, length=N)         # x koordinate mreže
+    y = range(minY, maxY, length=N)         # y koordinate mreže
+    return x' .* ones(N), ones(N)' .* y     # generirana mreža
 end
 
 
 
 #ulazni podaci
+n = 10               #broj pozitivnih naboja
+m = 10               #broj negativnih naboja
+maksNaboj = 30       #maksimalna apsolutna vrijednost naboja
+maksUdaljenost = 2   #maksimalna apsolutna udaljnost naboja od ishodišta
+N = 25               #gustoća koordinatne mreže
 
 
+# generiranje koordinata naboja:
+# prvih n za pozitivne, a ostali za negativne
+koordinateNaboja = rand(Uniform(-maksUdaljenost, maksUdaljenost), n + m, 2)
 
-koordinateNaboja = rand(Uniform(-1, 1), 10, 2)
-iznosNaboja = rand(Uniform(-30, 30), 10)
+# slučajno izgenerirane vrijednosti pozitivnih i negativnih naboja 
+iznosNaboja = rand(Uniform(0, maksNaboj))
 
-koordinateNaboja[2, 2]
-
-
-# !!MODEL - funkcije i matematika!!
 # definicija mreže
-# granice i gustoća mreže
-N = 25
-minX = minimum(koordinateNaboja[:, 1]) * 2
-maxX = maximum(koordinateNaboja[:, 1]) * 2
-minY = minimum(koordinateNaboja[:, 2]) * 2
-maxY = maximum(koordinateNaboja[:, 2]) * 2
-
-x = range(minX, maxX, length=N)     # x koordinate točaka
-y = range(minY, maxY, length=N)     # y koordinate točaka
-
-xG = x' .* ones(N)                  # mreža x koordinata
-yG = ones(N)' .* y                  # mreža y koodrinata
+xG, yG = mreza(koordinateNaboja, N)
 
 
 
-Ex = zeros(N, N)
-Ey = zeros(N, N)
+
+
 #izračun vektora polja
+E, Ex, Ey = elektricnoPolje(koordinateNaboja, iznosNaboja, xG, yG, N, n, m)
 
-Ex, Ey = elektricnoPolje(koordinateNaboja, iznosNaboja, xG, yG, N)
-
-#rezultat - izračun ukupnog polja
-E = sqrt.(Ex .^ 2 + Ey .^ 2)
-
-u = Ex ./ E   # normiranje vrijednosti polja
-v = Ey ./ E
+E
+Ex
+Ey
 
 #vizualizacija
 using Plots
 gr()
 
+# normiranje vrijednosti polja
+u = Ex ./ E
+v = Ey ./ E
+
 # pozicioniranje naboja u plot
-Plots.scatter(koordinateNaboja[:, 1], koordinateNaboja[:, 2], label="Naboj", aspect_ratio=:equal)
+Plots.scatter(koordinateNaboja[1:n, 1], koordinateNaboja[1:n, 2], label="Pozitivni naboj", aspect_ratio=:equal)
+Plots.scatter!(koordinateNaboja[n+1:n+m, 1], koordinateNaboja[n+1:n+m, 2], label="Negativni naboj", aspect_ratio=:equal)
 
 for i = 1:N
     display(quiver!(xG[i, :], yG[i, :], quiver=(u[i, :] / 10, v[i, :] / 10)))
 end
-
-# random koordinate u vektori i podešavanje mreže prema tome, funkcije
-# izborni zadatak: napraviti za više naboja (skini Distributu)
