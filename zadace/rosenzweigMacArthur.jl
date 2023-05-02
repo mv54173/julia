1584ed6a8bf89e784053b9d6ac3dd72d0e5d413a
Marko Varga <marko.varga@fer.hr>
2023-05-02 14:53:40 +0200

    drugi ciklus DZ

---
+++ zadace/rosenzweigMacArthur.jl
@@ -0,0 +1,53 @@
+# opći model interakcije grabežljivac-plijen
+
+using DifferentialEquations
+
+Rosenzweig_MacArthur = function (du, u, p, t)
+    alpha, beta, gamma, delta, K, b = p
+    du[1] = u[1] * (alpha * (1 - u[1] / K) - beta * (u[2] / (b + u[1])))
+    du[2] = -u[2] * (gamma - delta * (u[1] / (b + u[1])))
+end
+
+#= zadani parametri
+## konstante
+alpha = 0.9
+beta = 1
+gamma = 0.2
+delta = 0.3
+K = 100
+b = 10
+=#
+alpha = 1.2
+beta = 1
+gamma = 0.2
+delta = 0.3
+K = 100
+b = 20
+p = (alpha, beta, gamma, delta, K, b)
+## početni uvjeti
+u0 = [20.0; 10.0]
+tspan = (0.0, 300)
+## rješavanje problema
+problem = ODEProblem(Rosenzweig_MacArthur, u0, tspan, p)
+sol = solve(problem)
+## virtualizacija
+using Plots;
+gr();
+plot(sol, vars=(0, 1), title="grabežljivac plijen RM model", label="plijen", legend=:topright)
+plot!(sol, vars=(0, 2), label="grabežljivac", legend=:topright)
+plot!(xlab="vrijeme", ylab="populacija")
+#plot(sol, vars=(1, 2), title="grabežljivci po plijenu")
+
+
+# komentari:
+# Za zadane konstante i parametre dolazi do greške u modelu
+# gdje se bilježi pad populacije plijena na 0 jedinki.
+# Zanimljiv ulaz je P = (2.2, 0.7, 0.2, 0.3, 50, 10) gdje 
+# nakon određenog vreme broj jedinki grabežljivaca pređe broj
+# jedinki plijena i tada oscilira nalik na lagano prigušeni 
+# "ukošeni" sinus u pomaku na broj jedinki plijena koji oscilira na sličan način
+# Za P = (1.2, 0.7, 0.2, 0.3, 50, 10) prvo bilježimo nagli rast jedinki plijena
+# kojeg u pomaku prati "eksponencijalni" rast jedinki grabežljivaca što dovodi do pada
+# broja jedinki plijena na otpriliku polovicu kapaciteta okoliša. Tada se postiže
+# prigušena oscilacija populacija oko približno jednakog broja s tim da je amplituda
+# populacije plijena veća.
---
+++ zadace/sirDZ.jl
@@ -0,0 +1,123 @@
+using DifferentialEquations
+using Plots
+
+SIR_osnovni = function (du, u, p, t)
+    beta, gamma = p
+    du[1] = -beta * u[1] * u[2]
+    du[2] = beta * u[1] * u[2] - gamma * u[2]
+    du[3] = u[2] * gamma
+end
+
+SIS = function (du, u, p, t)
+    beta, gamma = p
+    du[1] = u[2] * gamma - beta * u[1] * u[2]
+    du[2] = beta * u[1] * u[2] - gamma * u[2]
+end
+
+SIRS = function (du, u, p, t)
+    beta, gamma, delta = p
+    du[1] = u[3] * delta - beta * u[1] * u[2]
+    du[2] = beta * u[1] * u[2] - gamma * u[2]
+    du[3] = u[2] * gamma - delta * u[3]
+end
+
+SIR_demografija = function (du, u, p, t)
+    beta, gamma, mi, ni = p
+    du[1] = -beta * u[1] * u[2] + ni * u[4] - mi * u[1]
+    du[2] = beta * u[1] * u[2] - gamma * u[2] - mi * u[2]
+    du[3] = u[2] * gamma - mi * u[3]
+    u[4] = u[1] + u[2] + u[3]
+end
+
+SIR_demo_vakc = function (du, u, p, t)
+    beta, gamma, mi, ni, kapa = p
+    du[1] = -beta * u[1] * u[2] + (ni - kapa) * u[4] - mi * u[1]
+    du[2] = beta * u[1] * u[2] - gamma * u[2] - mi * u[2]
+    du[3] = u[2] * gamma - mi * u[3]
+    u[4] = u[1] + u[2] + u[3]
+end
+
+SEIR = function (du, u, p, t)
+    beta, gamma, mi, ni, kapa, nu = p
+    du[1] = -beta * u[1] * u[3] + (ni - kapa) * u[5] - mi * u[1]
+    du[2] = beta * u[1] * u[3] - nu * u[2] - mi * u[2]
+    du[3] = beta * u[1] * u[3] - gamma * u[3] - mi * u[3]
+    du[4] = u[3] * gamma - mi * u[4]
+    u[5] = u[1] + u[2] + u[3] + u[4]
+end
+
+## ulazni podaci
+S = 4088000 # broj podložnih bolesti
+I = 1 # broj zaraženih
+R = 0 # broj oporavljenih
+t_oporavka = 10 # koliko dana je osoba bolesna
+N = S + I + R # ukupan broj ljudi
+k = 12 # broj kontakata na dan
+b = 0.05
+beta = (k * b) / N # koeficijent zaraze
+gamma = 1 / t_oporavka # koeficijent oporavka
+tspan = (0.0, 25 * 7) # vrijeme gledamo u danima, g
+
+
+#=
+#SIR_osnovni
+p = (beta, gamma)
+u0 = [S; I; R] # podešavanje početnih uvjeta
+problem = ODEProblem(SIR_osnovni, u0, tspan, p)
+=#
+#=
+#SIS
+p = (beta, gamma)
+u0 = [S; I] # podešavanje početnih uvjeta
+problem = ODEProblem(SIS, u0, tspan, p)
+=#
+#=
+#SIRS
+delta = 1/30
+p = (beta, gamma, delta)
+u0 = [S; I; R] # podešavanje početnih uvjeta
+problem = ODEProblem(SIRS, u0, tspan, p)
+=#
+#=
+#SIR + demografija
+mi = 0.0129
+ni = mi
+p = (beta, gamma, mi, ni)
+u0 = [S; I; R; N] # podešavanje početnih uvjeta
+problem = ODEProblem(SIR_demografija, u0, tspan, p)
+=#
+
+#=
+#SIR + demografija + cijepljenje
+mi = 0.0129
+ni = mi
+kapa = ni*323085/N
+p = (beta, gamma, mi, ni, kapa)
+u0 = [S; I; R; N] # podešavanje početnih uvjeta
+problem = ODEProblem(SIR_demo_vakc, u0, tspan, p)
+=#
+#=
+#vizualizacija
+sol = solve(problem)
+plot(sol, vars=(0, 1), title="oboljeli od gripe", label="podložni", legend=:topright)
+plot!(sol, vars=(0, 2), title="oboljeli od gripe", label="zaraženi", legend=:topright)
+plot!(sol, vars=(0, 3), title="oboljeli od gripe", label="oporavljeni", legend=:topright)
+=#
+
+
+#SEIR
+ni = 0.009
+mi = ni
+kapa = ni * 323085 / N
+nu = 1
+E = 0
+p = (beta, gamma, mi, ni, kapa, nu)
+u0 = [S; E; I; R; N] # podešavanje početnih uvjeta
+problem = ODEProblem(SEIR, u0, tspan, p)
+
+
+sol = solve(problem)
+plot(sol, vars=(0, 1), title="oboljeli od gripe", label="podložni", legend=:topright)
+plot!(sol, vars=(0, 2), title="oboljeli od gripe", label="asimptomatski", legend=:topright)
+plot!(sol, vars=(0, 3), title="oboljeli od gripe", label="zaraženi", legend=:topright)
+plot!(sol, vars=(0, 4), title="oboljeli od gripe", label="oporavljeni", legend=:topright)
